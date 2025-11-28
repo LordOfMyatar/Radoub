f7bf14e5befbf378f98d5985fedcf1169fe76b73
LordOfMyatar <lordofmyatar@gmail.com>
2025-11-28 13:04:59 -0800

    [UITests] feat: Show failed tests in run-tests.ps1 output
    Adds display of failed test names after test run completes.

--- Radoub.UITests/run-tests.ps1
+++ Radoub.UITests/run-tests.ps1
@@ -4,5 +4,7 @@ $outputFile = "Radoub.UITests\TestOutput\test_$timestamp.output"
 dotnet test Radoub.UITests --logger "console;verbosity=detailed" 2>&1 | Out-File -FilePath $outputFile
 
 Write-Host "Test output saved to: $outputFile"
+Write-Host "`nFailed tests:"
+Get-Content $outputFile | Select-String -Pattern "\[FAIL\]" | ForEach-Object { $_.Line }
 Write-Host "`nLast 7 lines of output:"
 Get-Content $outputFile -Tail 7
